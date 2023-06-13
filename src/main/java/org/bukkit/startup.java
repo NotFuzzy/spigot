a5f6c499853e493badf261b80e3beb42d604f3d8
ZyphIsCool <ll821301@gmail.com>
2023-06-13 15:47:44 -0400

    Update

--- src/main/java/org/bukkit/startup.java
+++ src/main/java/org/bukkit/startup.java
@@ -4,7 +4,7 @@ import static org.bukkit.Bukkit.getServer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class startup extends JavaPlugin {
-    public void loadTeams() {
+    public void onEnable() {
         getServer().getPluginManager().registerEvents(new optimizer(), this);
     }
 }
